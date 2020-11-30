dd8e64879f897037b49b10cb74c94cf2548236ac
oliver fu <ofu997@gmail.com>
2020-11-29 22:45:33 -0800

    mark orders as delivered

--- frontend/src/screens/OrderScreen.js
+++ frontend/src/screens/OrderScreen.js
@@ -6,10 +6,10 @@ import { Button, Row, Col, ListGroup, Image, Card } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux';
 import Message from '../components/Message'
 import Loader from '../components/Loader'
-import { getOrderDetails, payOrder } from '../actions/orderActions'
-import { ORDER_PAY_RESET } from '../constants/orderConstants'
+import { deliverOrder, getOrderDetails, payOrder } from '../actions/orderActions'
+import { ORDER_PAY_RESET, ORDER_DELIVER_RESET } from '../constants/orderConstants'
 
-const OrderScreen = ({ match }) => {
+const OrderScreen = ({ match, history }) => {
   const orderId = match.params.id
 
   const [sdkReady, setSdkReady] = useState(false)
@@ -21,6 +21,12 @@ const OrderScreen = ({ match }) => {
 
   const orderPay = useSelector(state => state.orderPay)
   const { loading:loadingPay, success:successPay } = orderPay
+
+  const orderDeliver = useSelector(state => state.orderDeliver)
+  const { loading:loadingDeliver, success:successDeliver } = orderDeliver
+
+  const userLogin = useSelector(state => state.userLogin)
+  const { userInfo } = userLogin
 
   if (!loading) {
     // calculate prices 
@@ -42,6 +48,9 @@ const OrderScreen = ({ match }) => {
 
   // as seen on video 63
   useEffect(() => {
+    if (!userInfo) {
+      history.push('/login')
+    }
     const addPayPalScript = async () => {
       const { data: clientId } = await axios.get('/api/config/paypal')
       console.log(clientId)
@@ -55,7 +64,8 @@ const OrderScreen = ({ match }) => {
       document.body.appendChild(script)
     }
 
-    if (!order || successPay) {
+    if (!order || successPay || successDeliver) {
+      dispatch({ type: ORDER_DELIVER_RESET })
       dispatch({ type: ORDER_PAY_RESET })
       dispatch(getOrderDetails(orderId))
     } else if (!order.isPaid) {
@@ -65,11 +75,15 @@ const OrderScreen = ({ match }) => {
         setSdkReady(true)
       }
     }
-  }, [dispatch, orderId, successPay, order])
+  }, [dispatch, orderId, successPay, successDeliver, order])
 
   const successPaymentHandler = paymentResult => {
     console.log(paymentResult) 
     dispatch(payOrder(orderId, paymentResult))
+  }
+
+  const deliverHandler = () => {
+    dispatch(deliverOrder(order))
   }
 
   return loading ? <Loader /> : error ? <Message variant='danger'>{error}</Message> :
@@ -176,6 +190,18 @@ const OrderScreen = ({ match }) => {
                       onSuccess={successPaymentHandler} 
                     />
                   )}
+                </ListGroup.Item>
+              )}
+              {loadingDeliver && <Loader />}
+              {userInfo && userInfo.isAdmin && order.isPaid && !order.isDeliver && (
+                <ListGroup.Item>
+                  <Button
+                    type='button'
+                    className='btn btn-block'
+                    onClick={deliverHandler} 
+                  >
+                    Mark as delivered 
+                  </Button>
                 </ListGroup.Item>
               )}
             </ListGroup>
